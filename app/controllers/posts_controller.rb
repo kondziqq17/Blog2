fbe81bb57e97088115308792150439ec7ea08902
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2013-09-08 12:34:05 +0200

    Add proper indention and fix something ;

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,44 @@
+#coding: utf-8
 class PostsController < ApplicationController
-def new
-  @post = Post.new
-end
- 
-def create
-  @post = Post.new(params[:post].permit(:title, :text))
- 
-  if @post.save
-    redirect_to @post
-  else
-    render 'new'
+
+  def new
+    @post = Post.new
   end
-end
 
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-   def index
+  def index
     @posts = Post.all
   end
 
-def show
-  @post = Post.find(params[:id])
-end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
   private
 
-    def post_params
-      params.require(:post).permit(:title, :text)
-   end
- end
-
- def edit
-  @post = Post.find(params[:id])
-end
-
-def update
-  @post = Post.find(params[:id])
- 
-  if @post.update(params[:post].permit(:title, :text))
-    redirect_to @post
-  else
-    render 'edit'
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
+
 end
